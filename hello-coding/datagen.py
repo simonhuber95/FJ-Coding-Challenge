047ba65700d514b6183cb200031818fb2cbbd428
Simon Huber <33151777+simonhuber95@users.noreply.github.com>
2021-06-23 12:29:44 +0200

    updata data generation

--- hello-coding/datagen.py
+++ hello-coding/datagen.py
@@ -2,7 +2,7 @@ import random
 
 def genererate_data(n, distr = None):
     """
-    Generates Data Points from the distribution
+    Generates Data Points from the distribution. 
     Args:
         n (int): number of points which are drawn in total
         distr (float): balance between correct and incorrect data points (y_true and y_false)
@@ -11,7 +11,9 @@ def genererate_data(n, distr = None):
         true_data, false_data (list): Returns two Lists filled with tuples of x and y coordinates between [0,1]
     """
     rand_percent = distr if distr and 0<= distr <= 1 else random.random()
-    true_data = [(random.randrange(0, 50)/100,random.randrange(0, 50)/100) for x in range(round(n*rand_percent))]
-    false_data = [(random.randrange(50, 100)/100,random.randrange(50, 100)/100) for x in range(round(n*(1-rand_percent)))]
+    true_data = [(round(random.randrange(50, 150)/100 -1, 2), round(random.randrange(50, 150)/100 - 1, 2)) for x in range(round(n*rand_percent))]
+    false_data = [(round(random.choice([random.randrange(0, 50)/100 - 1, random.randrange(150, 200)/100 - 1]),2), round(random.choice([random.randrange(0, 50)/100 - 1, random.randrange(150, 200)/100 - 1]),2)) for x in range(round(n*(1-rand_percent)))]
     return true_data, false_data
+
+
 
